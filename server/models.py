f0e800f25fd4a1c69dd016f85a4fc203e47b1d3c
mburugudestinygakenga <mburugugakenga@gmail.com>
2025-02-04 22:54:05 +0300

    add item 2

--- server/models.py
+++ server/models.py
@@ -44,3 +44,20 @@ class Item(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"<Item {self.id}, {self.name}, {self.price}>"
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    serialize_rules = ("-customer.reviews", "-item.reviews")
+
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
+
+    customer = db.relationship("Customer", back_populates="reviews")
+    item = db.relationship("Item", back_populates="reviews")
+
+    def __repr__(self):
+        return f"<Review {self.id}, {self.comment}, {self.customer.name}, {self.item.name}>"
